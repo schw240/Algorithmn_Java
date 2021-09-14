4116bd19dc8a1601ed15ecbd3af49abebc136eed
김한주 <schw240@gmail.com>
2021-09-14 22:28:51 +0900

    java문제풀이

---
+++ algo_hj/src/프로그래머스/고득점kit/동적계획법/N으로표현.java
@@ -0,0 +1,23 @@
+package 프로그래머스.고득점kit.동적계획법;
+
+public class N으로표현 {
+	
+	public static int solution(int N, int number) {
+		int ans = 0;
+		
+		// N으로 number 만들고 만드는데 필요한 최소 횟수 return
+		return ans
+		
+		
+		return ans;
+	}
+	
+	public static void main(String[] args) {
+		int N = 5;
+//		int N = 2;
+		int number = 12;
+//		int number = 11;
+		
+		System.out.println(solution(N, number));
+	}
+}
